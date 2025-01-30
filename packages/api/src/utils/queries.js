c5cbe71394b8ff221b7337f876250cdf223fee9d
syeero7 <EmalHasitha@proton.me>
2025-01-31 05:24:27 +0530

    feat: add queries for CRUD operations related to posts & comments

--- packages/api/src/utils/queries.js
+++ packages/api/src/utils/queries.js
@@ -1,7 +1,6 @@
 import prisma from "../config/prismaClient.js";
 
 export const getUserById = async (id) => {
-  id = parseInt(id);
   return await prisma.user.findUnique({ where: { id } });
 };
 
@@ -12,4 +11,80 @@ export const getUserByEmail = async (email) => {
 export const createUser = async (email, password, isAuthor) => {
   isAuthor = isAuthor === "true";
   await prisma.user.create({ data: { email, password, isAuthor } });
+};
+
+export const createPost = async (
+  title,
+  content,
+  createdAt,
+  isPublished,
+  authorId
+) => {
+  isPublished = isPublished === "true";
+
+  await prisma.post.create({
+    data: {
+      title,
+      content,
+      createdAt,
+      isPublished,
+      author: { connect: { id: authorId } },
+    },
+  });
+};
+
+export const updatePostPublishedStatus = async (isPublished, postId) => {
+  postId = parseInt(postId);
+  isPublished = isPublished === "true";
+  await prisma.post.update({ where: { id: postId }, data: { isPublished } });
+};
+
+export const deletePost = async (id) => {
+  id = parseInt(id);
+  await prisma.post.delete({ where: { id } });
+};
+
+export const updatePost = async (id, title, content, createdAt) => {
+  id = parseInt(id);
+  await prisma.post.update({
+    where: { id },
+    data: { title, content, createdAt },
+  });
+};
+
+export const getPostById = async (id, includeComments = false) => {
+  id = parseInt(id);
+  return await prisma.post.findUnique({
+    where: { id },
+    include: { comments: includeComments },
+  });
+};
+
+export const getAllPostsByAuthorId = async (authorId) => {
+  return prisma.post.findMany({ where: { authorId } });
+};
+
+export const getAllPublishedPosts = async () => {
+  return prisma.post.findMany({ where: { isPublished: true } });
+};
+
+export const createComment = async (authorId, postId, content) => {
+  postId = parseInt(postId);
+  return await prisma.comment.create({
+    data: {
+      content,
+      post: { connect: { id: postId } },
+      author: { connect: { id: authorId } },
+    },
+  });
+};
+
+export const deleteComment = async (id) => {
+  id = parseInt(id);
+  await prisma.comment.delete({ where: { id } });
+};
+
+export const updateComment = async (id, content) => {
+  id = parseInt(id);
+  return await prisma.comment.update({ where: { id }, data: { content } });
 };
