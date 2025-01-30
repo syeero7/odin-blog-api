7e832f2271f8f10f50baeea1456c53d55a42d171
syeero7 <EmalHasitha@proton.me>
2025-01-30 16:45:21 +0530

    feat: add configuration for passport authentication

---
+++ packages/api/src/config/passport.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import passport from "passport";
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
+import { Strategy as LocalStrategy } from "passport-local";
+import { getUserByEmail, getUserById } from "../utils/queries.js";
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await getUserByEmail(email);
+        if (!user) return done(null, false, { message: "User not found" });
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      secretOrKey: process.env.SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (payload, done) => {
+      try {
+        console.log(payload);
+        const user = await getUserById(payload.userId);
+
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
