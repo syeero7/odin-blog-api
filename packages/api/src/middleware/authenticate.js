7928d524e5a2b5a1ddfeac745b7e845e3cb4526b
syeero7 <EmalHasitha@proton.me>
2025-01-31 06:46:26 +0530

    fix: enable GET requests without user authentication

--- packages/api/src/middleware/authenticate.js
+++ packages/api/src/middleware/authenticate.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 
 const authenticate = (req, res, next) => {
+  if (!req.user && req.method === "GET") return next();
   passport.authenticate("jwt", { session: false })(req, res, next);
 };
 
