a0ede00f17d88d5220dbd09603b986284da6c363
syeero7 <EmalHasitha@proton.me>
2025-02-04 01:15:55 +0530

    build: configure proxy

--- packages/viewer-app/vite.config.js
+++ packages/viewer-app/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const API_URL = env.API_URL;
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          secure: API_URL.match(/https/) ? true : false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
