ebcb2f1d668836740da2d0093141e3bd57929048
syeero7 <EmalHasitha@proton.me>
2025-02-05 04:29:33 +0530

    fix: update loginAction function to return the response

--- packages/viewer-app/src/components/LoginForm/action.js
+++ packages/viewer-app/src/components/LoginForm/action.js
@@ -1,9 +1,8 @@
-import { redirect } from "react-router-dom";
 import { handleLogin } from "../../utils/api";
 
 export const loginAction = async ({ request }) => {
   const formData = await request.formData();
   const body = Object.fromEntries(formData);
-  await handleLogin(body);
-  return redirect("/");
+  const response = await handleLogin(body);
+  return response.json();
 };
