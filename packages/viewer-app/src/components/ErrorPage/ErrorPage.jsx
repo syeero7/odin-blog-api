3e5cd3e5e38727475870633ab27386185302e600
syeero7 <EmalHasitha@proton.me>
2025-02-06 13:54:25 +0530

    feat: style ErrorPage component

--- packages/viewer-app/src/components/ErrorPage/ErrorPage.jsx
+++ packages/viewer-app/src/components/ErrorPage/ErrorPage.jsx
@@ -1,18 +1,21 @@
 import { useRouteError, Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 
 function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {error.status} {error.statusText || error.message}
-      </p>
-      <p>
-        Back to <Link to="/">homepage</Link>
-      </p>
+    <div className={styles.container}>
+      <article>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p className={styles.error}>
+          {error.status} {error.statusText || error.message}
+        </p>
+        <p className={styles.link}>
+          Back to <Link to="/">homepage</Link>
+        </p>
+      </article>
     </div>
   );
 }
