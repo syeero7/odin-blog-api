36f4aceca167aa85458fa350f9adc99cc2dc0854
syeero7 <EmalHasitha@proton.me>
2025-02-07 15:30:30 +0530

    refactor: integrate ActionButtons component within PostList component

--- packages/creator-app/src/components/PostList/PostList.jsx
+++ packages/creator-app/src/components/PostList/PostList.jsx
@@ -1,12 +1,7 @@
-import {
-  Link,
-  useLoaderData,
-  Form,
-  useFetcher,
-  useNavigate,
-} from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import propTypes from "prop-types";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import ActionButtons from "../ActionButtons/ActionButtons";
 
 function PostList() {
   const { posts } = useLoaderData();
@@ -30,8 +25,6 @@ function PostList() {
 export default PostList;
 
 function Post({ id, title, content, isPublished }) {
-  const navigate = useNavigate();
-
   return (
     <article>
       <Link to={`posts/${id}`}>
@@ -42,13 +35,7 @@ function Post({ id, title, content, isPublished }) {
         </p>
       </Link>
 
-      <div>
-        <button onClick={() => navigate(`${id}/edit`)}>Edit</button>
-        <TogglePostState state={isPublished} postId={id} />
-        <Form action={`${id}/delete`} method="delete">
-          <button type="submit">Delete</button>
-        </Form>
-      </div>
+      <ActionButtons id={id} isPublished={isPublished} />
     </article>
   );
 }
@@ -58,29 +45,4 @@ Post.propTypes = {
   title: propTypes.string.isRequired,
   content: propTypes.string.isRequired,
   isPublished: propTypes.bool.isRequired,
-};
-
-function TogglePostState({ state, postId }) {
-  const fetcher = useFetcher();
-  const isPublished = fetcher.formData
-    ? fetcher.formData.get("isPublished") === "true"
-    : state;
-
-  return (
-    <fetcher.Form method="put" action={`${postId}/update-state`}>
-      <button
-        type="submit"
-        name="isPublished"
-        value={isPublished ? "true" : "false"}
-        aria-label={isPublished ? "unpublish the post" : "publish the post"}
-      >
-        {isPublished ? "unpublish" : "publish"}
-      </button>
-    </fetcher.Form>
-  );
-}
-
-TogglePostState.propTypes = {
-  state: propTypes.bool.isRequired,
-  postId: propTypes.number.isRequired,
 };
