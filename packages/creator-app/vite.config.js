01df3e95fb3f55618dc7673645d1aa1a6ac22cf7
syeero7 <EmalHasitha@proton.me>
2025-02-07 12:34:43 +0530

    build: configure proxy

--- packages/creator-app/vite.config.js
+++ packages/creator-app/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
+import { dirname } from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const envDir = dirname(dirname(process.cwd()));
+  const env = loadEnv(mode, envDir, "");
+  const API_URL = env.API_URL;
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          secure: API_URL.match(/https/) ? true : false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
--- packages/viewer-app/vite.config.js
+++ packages/viewer-app/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import process from "process";
+import { dirname } from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const envDir = dirname(dirname(process.cwd()));
+  const env = loadEnv(mode, envDir, "");
   const API_URL = env.API_URL;
+
   return {
     plugins: [react()],
     server: {
