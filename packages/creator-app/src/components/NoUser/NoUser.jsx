d70ffda61abf09b147d5dc1d1eea8676a92f6c3f
syeero7 <EmalHasitha@proton.me>
2025-02-09 18:23:19 +0530

    feat: style NoUser component

--- packages/creator-app/src/components/NoUser/NoUser.jsx
+++ packages/creator-app/src/components/NoUser/NoUser.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, Navigate } from "react-router-dom";
 import { useAuth } from "../AuthProvider/AuthProvider";
+import styles from "./NoUser.module.css";
 
 function NoUser() {
   const navigate = useNavigate();
@@ -8,12 +9,16 @@ function NoUser() {
   if (token != null) return <Navigate to="/posts" replace />;
 
   return (
-    <div>
-      <h1>Access Restricted</h1>
-      <p>Please log in to continue</p>
+    <div className={styles.container}>
+      <div>
+        <h1>Access Restricted</h1>
+        <p>Please log in to continue</p>
 
-      <button onClick={() => navigate("/login")}>Login</button>
-      <button onClick={() => navigate("/register")}>Sign Up</button>
+        <div className={styles.buttons}>
+          <button onClick={() => navigate("/login")}>Login</button>
+          <button onClick={() => navigate("/register")}>Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 }
