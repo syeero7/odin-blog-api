e621695b1f03db4eccf80c2cea112985a4d36a66
syeero7 <EmalHasitha@proton.me>
2025-02-10 11:58:32 +0530

    build: reinitialize Vite configuration

--- packages/viewer-app/vite.config.js
+++ packages/viewer-app/vite.config.js
@@ -1,25 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import process from "process";
-import { dirname } from "path";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const envDir = dirname(dirname(process.cwd()));
-  const env = loadEnv(mode, envDir, "");
-  const API_URL = env.API_URL;
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: API_URL,
-          changeOrigin: true,
-          secure: API_URL.match(/https/) ? true : false,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
 });
