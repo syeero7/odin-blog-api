867704680517783454353fba52548e36d9542158
syeero7 <EmalHasitha@proton.me>
2025-02-10 21:16:37 +0530

    feat: implement logging error messages in ErrorPage component

--- packages/creator-app/src/components/ErrorPage/ErrorPage.jsx
+++ packages/creator-app/src/components/ErrorPage/ErrorPage.jsx
@@ -4,13 +4,16 @@ import styles from "./ErrorPage.module.css";
 function ErrorPage() {
   const error = useRouteError();
 
+  console.error(`status: ${error.status} ${error.statusText}`);
+  if (error.data?.message) console.error(`error: ${error.data.message}`);
+
   return (
     <div className={styles.container}>
       <article>
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p className={styles.error}>
-          {error.status} {error.statusText || error.message}
+          {error.status} {error.statusText}
         </p>
         <p className={styles.link}>
           Back to <Link to="/">homepage</Link>
