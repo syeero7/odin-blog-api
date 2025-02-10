7e9332b6c62a1762863ab93013b6456ed68b0e22
syeero7 <EmalHasitha@proton.me>
2025-02-10 07:49:46 +0530

    feat: configure CORS

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 import "./config/passport.js";
 import routes from "./routes/index.js";
 import middleware from "./middleware/index.js";
+import { corsOptions } from "./config/cors.js";
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
