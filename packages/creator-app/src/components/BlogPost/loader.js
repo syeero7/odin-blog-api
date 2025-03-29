a06115e09363e6de9083329f7df3e1940b1195d9
syeero7 <EmalHasitha@proton.me>
2025-03-29 23:27:29 +0530

    chore: remove old files

--- packages/creator-app/src/components/BlogPost/loader.js
+++
@@ -1,7 +0,0 @@
-import { getPost } from "../../utils/api";
-
-export const postLoader = async ({ params }) => {
-  const postId = Number(params.postId);
-  const post = await getPost(postId);
-  return post.json();
-};
--- packages/creator-app/src/components/PostList/loader.js
+++
@@ -1,6 +0,0 @@
-import { getPosts } from "../../utils/api";
-
-export const postListLoader = async () => {
-  const posts = await getPosts();
-  return posts.json();
-};
