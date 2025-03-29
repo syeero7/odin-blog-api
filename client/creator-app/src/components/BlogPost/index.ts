be39a26e9b456ac1eb34c73964f2c715de25e5c2
syeero7 <EmalHasitha@proton.me>
2025-03-29 23:26:35 +0530

    feat: add blog post actions

--- client/creator-app/src/components/BlogPost/index.ts
+++ client/creator-app/src/components/BlogPost/index.ts
@@ -1,2 +1,3 @@
 export { default } from "./BlogPost";
 export { loader } from "./loader";
+export * as actions from "./actions";
--- client/creator-app/src/routes.tsx
+++ client/creator-app/src/routes.tsx
@@ -3,10 +3,13 @@ import Fallback from "@common/components/Fallback";
 import ErrorPage from "@common/components/ErrorPage";
 import LoginForm from "@common/components/LoginForm";
 import SignUpForm from "@common/components/SignUpForm";
-import * as actions from "@common/components/BlogPost/actions";
+import * as commentActions from "@common/components/BlogPost/actions";
 
 import PostList, { loader as postsLoader } from "./components/PostList";
-import BlogPost, { loader as blogPostLoader } from "./components/BlogPost";
+import BlogPost, {
+  loader as blogPostLoader,
+  actions as postActions,
+} from "./components/BlogPost";
 import CreatePost from "./components/CreatePost";
 import EditPost from "./components/EditPost";
 import Homepage from "./components/Homepage";
@@ -25,20 +28,26 @@ export const browserRouter = createBrowserRouter([
       {
         path: "posts",
         loader: postsLoader,
+        action: postActions.updatePostStatus,
         Component: PostList,
         HydrateFallback: Fallback,
       },
       { path: "posts/new", Component: CreatePost },
-      { path: "posts/:postId/update", Component: EditPost, loader: blogPostLoader },
       {
         path: "posts/:postId",
-        loader: blogPostLoader,
-        Component: BlogPost,
         HydrateFallback: Fallback,
         children: [
-          { path: "comments", action: actions.createComment },
-          { path: "comments/:commentId/update", action: actions.updateComment },
-          { path: "comments/:commentId/delete", action: actions.deleteComment },
+          {
+            index: true,
+            Component: BlogPost,
+            loader: blogPostLoader,
+            action: postActions.updatePostStatus,
+          },
+          { path: "update", Component: EditPost, loader: blogPostLoader },
+          { path: "delete", action: postActions.deletePost },
+          { path: "comments", action: commentActions.createComment },
+          { path: "comments/:commentId/update", action: commentActions.updateComment },
+          { path: "comments/:commentId/delete", action: commentActions.deleteComment },
         ],
       },
     ],
